0a198148c0e4ee8735c9cc8f85fba41c27f9aae0
lsjazz <osj7577@rit.edu>
2025-03-13 10:14:23 -0400

    copied code from the cs resource page and made slight modifications for our specific use

--- database/db.py
+++ database/db.py
@@ -1,3 +1,4 @@
+"""
 import psycopg2
 
 # Replace these values with your actual database connection details
@@ -30,4 +31,39 @@ try:
     connection.close()
 
 except Exception as error:
-    print(f"Error: {error}")
+    print(f"Error: {error}")"
+"""
+
+import psycopg2
+from sshtunnel import SSHTunnelForwarder
+
+username = "osj7577"
+password = "histet-Jubrih-5zygda"
+dbName = "p32001_19"
+
+
+try:
+    with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                            ssh_username=username,
+                            ssh_password=password,
+                            remote_bind_address=('127.0.0.1', 5432)) as server:
+        server.start()
+        print("SSH tunnel established")
+        params = {
+            'database': dbName,
+            'user': username,
+            'password': password,
+            'host': 'localhost',
+            'port': server.local_bind_port
+        }
+
+
+        conn = psycopg2.connect(**params)
+        curs = conn.cursor()
+        print("Database connection established")
+
+        #DB work here....
+
+        conn.close()
+except:
+    print("Connection failed")
