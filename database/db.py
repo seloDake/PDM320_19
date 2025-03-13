6a1545a365f9342f9064ca21d5db1e52ae013848
knn4602 <knn4602@rit.edu>
2025-03-12 22:45:26 -0400

    test

--- database/db.py
+++ database/db.py
@@ -3,9 +3,9 @@ import psycopg2
 # Replace these values with your actual database connection details
 host = "127.0.0.1"         # The host of the database
 port = "5432"              # The port (default: 5432)
-dbname = "p32001_19"    # The database name
-user = "knn4602"     # Your PostgreSQL username
-password = "Kainakaren112$" # Your PostgreSQL password
+dbname = ""    # The database name
+user = ""     # Your PostgreSQL username
+password = "Kainakaren1$" # Your PostgreSQL password
 
 # Establish a connection to the PostgreSQL database
 try:
@@ -19,6 +19,7 @@ try:
 
     # Create a cursor object to interact with the database
     cursor = connection.cursor()
+    print("Connection successful!")
 
     # Test the connection by running a query
     cursor.execute("SELECT version();")
