cd38cb24e235792a2d08f1a9d48eb0185cf65f3e
Christabel Osei <armahsarpomaa16@gmail.com>
2025-03-18 20:35:50 -0400

    Done

--- models/user.py
+++ models/user.py
@@ -101,9 +101,9 @@ class User:
             with cls.conn.cursor() as cursor:
                 # increment_user_id = cls.increment_counter_user_id()
                 cursor.execute("""
-                    INSERT INTO users (username, email, password, first_name, last_name,created_date)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, NULL)
-                """, (username, email, password, first_name, last_name,current_time))
+                    INSERT INTO users (username, email, password, first_name, last_name,creation_date)
+                    VALUES (%s, %s, %s, %s, %s, %s)
+                """, (username, email, first_name, last_name,password,current_time))
                 cls.conn.commit()
                 print("Your account has been created! Please sign in to access other functionalities.")
         except psycopg2.Error as e:
